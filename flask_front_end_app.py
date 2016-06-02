88676594ee54ab1bda1c4bd26bc44ddf8a321c4e
Jordan Rodak <jrodak@stanford.edu>
2016-06-02 13:18:01 -0700

    Added route for filtering data by protocol. Need to add some code so that more than just top level protocol is considered

--- flask_front_end_app.py
+++ flask_front_end_app.py
@@ -1,4 +1,4 @@
-import os, copy
+import os, copy, json
 from flask import Flask, jsonify, request, send_from_directory, make_response
 app = Flask(__name__, static_url_path='')
 
@@ -15,11 +15,17 @@ def send_assets(path):
 
 @app.route('/data/')
 def send_data():
-	return app.make_response(open('app/data/data.json').read())
+    return app.make_response(open('app/data/data.json').read())
+
+@app.route('/data/<path:protocol>/')
+def send_data_protcol(protocol):
+    with open('app/data/data.json') as data_file:
+        data_json = json.load(data_file)
+        return json.dumps([x for x in data_json if x['protocol'].lower() == protocol.lower()])
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5050))
-	app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
 
 
